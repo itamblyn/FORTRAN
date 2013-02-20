1f4dd58417e15c992fca59d4e6be7b3dadc42290
Isaac Tamblyn <itamblyn@pat.(none)>
2013-02-20 16:24:41 -0500

    removed simple script

--- potential.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python
-
-import numpy
-
-for r in numpy.arange(0.0,2.0,0.025):
-    v = numpy.sin(min(r - 1,numpy.pi/2))**2
-    dv = 2.*numpy.sin(min(r - 1,numpy.pi/2))*numpy.cos(min(r,numpy.pi/2))
-    print r,v,dv
